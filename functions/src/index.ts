cce3ac2ed4aee64187118a28c0a8cf15b8ba971f
unknown <dracsharp@gmail.com>
2020-09-23 12:12:20 +0100

    backend ready to launch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,8 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import { stripeKey } from "./stripe/stripeKey";
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(stripeKey);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +10,22 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
+console.log("stripe", stripe);
+
 // });
+//* API
+
+//* App config
+const app = express();
+
+//* MiddleWares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+//* API routes
+app.get("/", (request: any, response: any) =>
+	response.status(200).send("hello world")
+);
+
+//* Listen command
+exports.api = functions.https.onRequest(app);
---
+++ functions/src/stripe/stripeKey.ts
@@ -0,0 +1,1 @@
+export const stripeKey = "sk_test_AqPkvYeBfeDrtgzso1Ztf0vH004p5nLJuo";
